0667db3f0b74ed6bc559dcaedd86756dd06e2113
MedinaSilva <k.medinasilva1234@gmail.com>
2023-03-21 19:08:27 -0600

    Revision de codigo

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,9 +4,10 @@ const $n = document.querySelector('name');
 const $b = document.querySelector('#blog');
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+async function displayUser(username) {/* Se agrega async porque debajo tenemos un await que indica que ess una funcion asyncrona */
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
+  const data = await response.json();/*  */
   console.log(data);
   $n.textContent = '${data.name}';
   $b.textContent = '${data.blog}';
@@ -16,7 +17,8 @@ function displayUser(username) {
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  const $n = document.getElementById(naame);
+  $n.textContent = `Algo salió mal: ${err}`
 }
 
 displayUser('stolinski').catch(handleError);
